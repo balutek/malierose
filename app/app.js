8c9560574c97996edde9fc610c040ef21887b5e3
Paweł Łabuda <emc206@gmail.com>
2015-12-09 22:19:03 +0100

    gallery refined

--- app/app.js
+++ app/app.js
@@ -1,9 +1,37 @@
-require('angular');
+var angular = require('angular');
 var bootstrap = require('angular-ui-bootstrap');
+//var ngAnimate = require('angular-animate');
+var ngRoute = require('angular-route');
 
-var app = angular.module('app', [bootstrap]);
+var app = angular.module('app', [bootstrap, ngRoute]);
 
-var MainController = require('./controllers/MainController');
 
-app.controller('MainController', ['$scope', MainController]);
+var NavbarController = require('./controllers/NavbarController');
+var CarouselController = require('./controllers/CarouselController');
+
+var AboutController = require('./controllers/views/AboutController');
+var BuildingHousesController = require('./controllers/views/BuildingHousesController');
+
+app.controller('NavbarController', NavbarController);
+app.controller('CarouselController', CarouselController);
+
+app.controller('AboutController', AboutController);
+app.controller('BuildingHousesController', BuildingHousesController);
+
+app.config(['$routeProvider', function ($routeProvider) {
+
+  $routeProvider
+    .when('/about', {
+      templateUrl: 'views/about.html',
+      controller: 'AboutController'
+    })
+    .when('/buildingHouses', {
+      templateUrl: 'views/building_houses.html',
+      controller: 'BuildingHousesController'
+    })
+    .otherwise({
+      redirectTo: '/about'
+    });
+
+}]);
 
---
+++ app/controllers/CarouselController.js
@@ -0,0 +1,27 @@
+/**
+ * Created by pawel on 01.12.15.
+ */
+module.exports = function ($scope) {
+  $scope.carouselInterval = 5000000000;
+  $scope.noWrapSlides = false;
+
+  var slides = $scope.slides = [];
+
+  $scope.addSlide = function (imgPath, text)
+  {
+    slides.push({
+      image: imgPath,
+      text: text
+    });
+  };
+
+  $scope.addSlide("./img/20140704_144215.jpg", "img1");
+  $scope.addSlide("./img/20140728_145228.jpg", "img1");
+  $scope.addSlide("./img/20140730_082028.jpg", "img1");
+  $scope.addSlide("./img/20140913_181602.jpg", "img1");
+  $scope.addSlide("./img/20140913_181930.jpg", "img1");
+  $scope.addSlide("./img/20140914_164111.jpg", "img1");
+  $scope.addSlide("./img/20140923_145713.jpg", "img1");
+  $scope.addSlide("./img/20141001_131749.jpg", "img1");
+
+};
